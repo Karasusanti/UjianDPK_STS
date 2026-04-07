3f4765957ffaac2b46328af5e888185dcd2d0a03
zaidan <zaidzaid1111345@gmail.com>
2026-04-07 21:07:38 +0700

    Update login.js

--- JS/login.js
+++ JS/login.js
@@ -1,19 +1,20 @@
 function login() {
-    let name = document.getElementById("username").value.trim();
-    let name = document.getElementById("alamat").value.trim();
-    let name = document.getElementById("umur").value.trim();
+    let username = document.getElementById("username").value.trim();
+    let alamat = document.getElementById("alamat").value.trim();
+    let umur = document.getElementById("umur").value.trim();
 
-
-    if (name === "") {
-        alert("Masukkan nama dulu");
+    if (username === "" || alamat === "" || umur === "") {
+        alert("Semua data harus diisi!");
         return;
     }
 
-    localStorage.setItem("user", name);
+    // simpan ke localStorage
+    localStorage.setItem("user", username);
+    localStorage.setItem("alamat", alamat);
+    localStorage.setItem("umur", umur);
 
     window.location.href = "dashboard.html";
 }
-
 function handleEnter(e) {
     if (e.key === "Enter") {
         login();
